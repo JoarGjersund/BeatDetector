aa646f3a13e185a2fa81a0dea3931ed91a3a27df
Joar andreas Gjersund <joarandreasgjersund@Joars-MacBook-Air.local>
2021-05-12 20:02:39 +0200

    removable log

--- src/Beatdetector.h
+++ src/Beatdetector.h
@@ -4,7 +4,10 @@
 #include <Arduino.h>
 #include <Wire.h>
 // FHT, see http://wiki.openmusiclabs.com/wiki/ArduinoFHT
+#ifndef LOG_OUT
 #define LOG_OUT 1 // use the log output function
+#endif
+
 #define FHT_N 128 // amount of bins to use
 
 
